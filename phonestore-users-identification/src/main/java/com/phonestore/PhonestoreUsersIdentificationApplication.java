ab814f3cb184c49efbd405e132c5e3cf921b70b6
remiNdong <ndongremi@aol.fr>
2023-07-14 11:40:46 +0200

    first commit

---
+++ phonestore-users-identification/src/main/java/com/phonestore/PhonestoreUsersIdentificationApplication.java
@@ -0,0 +1,13 @@
+package com.phonestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonestoreUsersIdentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonestoreUsersIdentificationApplication.class, args);
+	}
+
+}
---
+++ phonestore-users-identification/src/test/java/com/phonestore/PhonestoreUsersIdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phonestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonestoreUsersIdentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
